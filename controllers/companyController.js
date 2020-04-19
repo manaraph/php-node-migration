6e061c9e11fc87851e948a25e67bfe9b239e31c0
manaraph <manaraph225@gmail.com>
2020-04-19 13:24:55 +0100

    Get companies

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,5 +1,22 @@
 const sql = require('../config/dbConfig');
 
+const getCompanies = (req, res, next) => {
+  sql.query("SELECT * FROM companies", (err, result) => {
+    if (err) {
+      console.log("error: ", err);
+      // next(err, null);
+      res.json({ error: 'Error fetching companies to the database' });
+    }
+
+    res.status(200).json({
+      message: 'success',
+      data: {
+        companies: result
+      }
+    });
+  });
+};
+
 const createCompany = (req, res, next) => {
   const { name, email, logo, website } = req.body;
   sql.query("INSERT INTO companies SET name = ?, email = ?, website = ?", [name, email, website], (err, result) => {
@@ -19,5 +36,6 @@ const createCompany = (req, res, next) => {
 };
 
 module.exports = {
+  getCompanies,
   createCompany,
 }
--- routes/companyRouter.js
+++ routes/companyRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const company = require('../controllers/companyController')
 
-router.get('/', company.createCompany);
+router.get('/', company.getCompanies);
 router.post('/', company.createCompany);
 
 module.exports = router;
