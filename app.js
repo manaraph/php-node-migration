d39ffd2b77bc6a650680503a0f0dba0b80548813
manaraph <manaraph225@gmail.com>
2020-04-20 09:30:51 +0100

    API Connection fixes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', `${process.env.FRONTENDURL || 'http://localhost:3000'}`);
+  res.setHeader('Access-Control-Allow-Origin', `${process.env.FRONTENDURL || 'http://localhost:5000'}`);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
