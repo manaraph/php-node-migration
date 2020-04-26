5784e7c43df577fbcd6726eb45910e6d351dd5f8
manaraph <manaraph225@gmail.com>
2020-04-26 19:30:31 +0100

    refactor for review creation

--- controllers/reviewControllers.js
+++ controllers/reviewControllers.js
@@ -1,20 +1,25 @@
 const sql = require('../config/dbConfig');
 
 const createReview = (req, res, next) => {
-  const { firstname, lastname, email, phone } = req.body;
-  const { companyId } = req.params;
-  sql.query("INSERT INTO business_reviews SET firstname = ?, lastname = ?, company = ?, email = ?, phone = ?", [firstname, lastname, companyId, email, phone ], (err, result) => {
+  const { businessId, reviewer, review } = req.body;
+  sql.query("INSERT INTO business_reviews SET businessId = ?, reviewer = ?, review = ?", [businessId, reviewer, review ], (err, result) => {
     if (err) {
       console.log("error: ", err);
       // next(err, null);
-      return res.status(400).json({ error: 'Error saving Employee to the database' });
+      return res.status(400).json({ error: 'Error saving review to the database' });
     }
 
     return res.status(200).json({
       message: 'success',
       data: {
-        employeeId: result.insertId
+        businessId, 
+        reviewer, 
+        review
       }
     });
   });
 };
+
+module.exports = {
+  createReview,
+}
